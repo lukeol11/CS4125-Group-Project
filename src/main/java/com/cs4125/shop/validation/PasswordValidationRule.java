0c042b3fd63d6a63f49a487ff7fefd13af87da64
Doctor-23815 <91544694+Doctor-23815@users.noreply.github.com>
2023-11-23 17:24:54 +0000

    Update PasswordValidationRule.java

--- src/main/java/com/cs4125/shop/validation/PasswordValidationRule.java
+++ src/main/java/com/cs4125/shop/validation/PasswordValidationRule.java
@@ -5,29 +5,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class PasswordValidationRule {
     public boolean validatePassword(String password) {
-        // Check if the password meets the length requirement (between 8 and 16 characters).
-        if (password.length() < 8 || password.length() > 16) {
-            return false; // Password length is invalid.
-        }
-
-        // Check if the password contains at least one uppercase letter.
-        if (!password.matches(".*[A-Z].*")) {
-            return false; // Password does not contain an uppercase letter.
-        }
-
-        // Check if the password contains at least one lowercase letter.
-        if (!password.matches(".*[a-z].*")) {
-            return false; // Password does not contain a lowercase letter.
-        }
-
-        // Check if the password contains at least one digit.
-        if (!password.matches(".*\\d.*")) {
-            return false; // Password does not contain a digit.
-        }
-
-        // Check if the password contains at least one special character.
-        if (!password.matches(".*[!@#$%^&*()].*")) {
-            return false; // Password does not contain a special character.
+        // Password must be between 8 and 16 characters,
+        // contain at least one uppercase letter, one lowercase letter,
+        // one digit, and one special character.
+        if (!password.matches("(?i)^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,16}$")) {
+            return false; // Password is invalid.
         }
 
         return true; // Password is valid.
